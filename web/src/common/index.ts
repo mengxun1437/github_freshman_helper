8295985f5cd57ecfd1dbdb13a253e6b618b342fa
mengxun1437@gmail.com <mengxun1437@gmail.com>
2022-02-06 21:03:57 +0800

    chore: quick fix

--- web/src/common/index.ts
+++ web/src/common/index.ts
@@ -1,4 +1,4 @@
-// export const BASE_SERVER_URL = 'http://api.mengxun.online/gfh'
-export const BASE_SERVER_URL = 'http://localhost:10310'
+export const BASE_SERVER_URL = 'http://api.mengxun.online/gfh'
+// export const BASE_SERVER_URL = 'http://localhost:10310'
 export const BASE_PROXY_URL = 'http://api.mengxun.online/proxy'
 export const BASE_QINIU_URL = 'http://qiniu-gfh.mengxun.online'
