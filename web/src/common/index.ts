5691f07803c1327af791f6128eac49a25e3a3c5c
王武俊 <wangwujun.mx@bytedance.com>
2023-02-16 18:01:14 +0800

    feat: add files

--- web/src/common/index.ts
+++
@@ -1,4 +0,0 @@
-
-export const BASE_SERVER_URL = 'http://localhost:10310'
-export const BASE_PROXY_URL = 'http://proxy.zerokirin.online'
-export const BASE_QINIU_URL = 'http://cdn-gfh.zerokirin.online'
